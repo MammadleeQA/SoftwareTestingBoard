a45317ddba1b56f524ece7edfbd1de8936e42aef
Shahin82 <shahin.rzayev@gmail.com>
2023-08-23 01:08:33 +0400

    Add files via upload

---
+++ CPSTP.java
@@ -0,0 +1,40 @@
+package StepDefinitions;
+
+import io.cucumber.core.internal.com.fasterxml.jackson.databind.ser.std.ObjectArraySerializer;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+import static POM.CPPOM.*;
+import static POM.ConditionOptionEnum.CLICKABLE;
+import static POM.ConditionOptionEnum.VISIBLE;
+import static POM.MWLPOM.*;
+import static POM.MWLPOM.WISHLIST_SHOWS_UP;
+
+public class CPSTP extends BaseMethods {
+
+    @And("Adding items to compare list and seeing if the list is filled")
+    public void CompareProductCheckUp() {
+        scrollByWeb(0,1800);
+        secondsWait(5);
+        getElement(ITEM10).click();
+        getElement(ITEM11).click();
+        explicitlyWait(LOGO2, VISIBLE, 10);
+        getElement(LOGO2).click();
+        secondsWait(5);
+        scrollByWeb(0,1800);
+        getElement(ITEM12).click();
+        getElement(ITEM13).click();
+        explicitlyWait(LOGO2, VISIBLE, 10);
+        getElement(LOGO2).click();
+        secondsWait(3);
+        getElement(COMPARE_LIST).click();
+        secondsWait(5);
+
+    }
+
+    @Given("My compare product items are visible")
+    public void CompareProductContent() {
+        explicitlyWait(PRESENT_1, VISIBLE, 5);
+        explicitlyWait(PRESENT_2, VISIBLE, 5);
+    }
+}
---
+++ MWLSTP.java
@@ -0,0 +1,40 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+import static DriverHook.DriverConfig.driver;
+import static POM.ConditionOptionEnum.CLICKABLE;
+import static POM.ConditionOptionEnum.VISIBLE;
+import static POM.FilterPOM.*;
+import static POM.FilterPOM.LOGIN_BTN;
+import static POM.MWLPOM.*;
+
+public class MWLSTP extends BaseMethods{
+
+    @And("Adding items to wish list and seeing if the list is filled")
+    public void WishListCheckUp() {
+        scrollByWeb(0,1800);
+        secondsWait(5);
+        getElement(ITEM0).click();
+        getElement(ITEM1).click();
+        explicitlyWait(LOGO, VISIBLE, 10);
+        getElement(LOGO).click();
+        secondsWait(5);
+        scrollByWeb(0,1800);
+        getElement(ITEM2).click();
+        getElement(ITEM3).click();
+        explicitlyWait(LOGO, VISIBLE, 10);
+        getElement(LOGO).click();
+        getElement(DROPDOWN).click();
+        secondsWait(5);
+        getElement(WISHLIST).click();
+        secondsWait(2);
+        getElement(LOGO).click();
+    }
+
+    @Given("My wishlist is visible")
+    public void WishListHasContent() {
+        explicitlyWait(WISHLIST_SHOWS_UP, VISIBLE, 10);
+    }
+}
